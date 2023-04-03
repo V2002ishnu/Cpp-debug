003794c10d91c51a10bf90e318acf5ac1e06625d
subhajit735 <subhajit735@gmail.com>
2023-04-04 02:48:28 +0530

    logicalproblem2

---
+++ logicalproblem02.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(char a, char b)
+{
+    // write the function here
+}
+
+int main()
+{
+    char a;
+    char b;
+    cout << "Enter the characters :";
+    cin >> a >> b;
+    if (check(a, b))
+    {
+        cout << "Yes" << endl;
+    }
+
+    else
+    {
+        cout << "No" << endl;
+    }
+    return 0;
+}
