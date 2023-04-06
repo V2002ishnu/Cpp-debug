53f7365ea88e3a3ca05c734856a7fa0250f7626f
V2002ishnu <kbhi50188@gmail.com>
2023-04-06 19:28:46 +0530

    implemented factorial function

--- recursioncreation.cpp
+++ recursioncreation.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 
 //create a recursive funtion of the following logic given in the main function.
+long long factorial(int n){
+    if(n <= 0 ) return 1;
+    return factorial(n-1) * n;
+}
 
 int main(){
     int n ;
     cout<<"Enter the number : ";
     cin>>n;
-    int ans = 1; 
-    for(int i = n ; i  >= 1 ;i--){
-        ans = ans * i;   
-    }
+    long long ans = factorial(n); 
+
     cout<<ans<<endl;
 }
